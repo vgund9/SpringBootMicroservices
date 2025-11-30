9d4ed5aa36540d26d425607bd82c7d26bf03f78b
vijaygmeughar <vijayg.meughar@gmail.com>
2025-11-30 23:34:28 +0530

    First Commit

---
+++ src/test/java/com/vvrp/demo/SpringMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vvrp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
